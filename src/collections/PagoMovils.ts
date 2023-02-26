0074a3aa6dacd23a6859083e5b3269601529fbeb
guillermoscript <arepayquezo@gmail.com>
2023-02-26 16:56:18 -0400

    pago movil added with field utility

--- src/collections/PagoMovils.ts
+++ src/collections/PagoMovils.ts
@@ -21,93 +21,13 @@ const PagoMovils: CollectionConfig = {
             required: true,
             label: 'Número de teléfono',
         },
-        // VenezuelanBanks(),
-        {
-            name: 'bank',
-            type: 'radio',
-            options: [
-                {
-                    label: 'Banco de Venezuela',
-                    value: 'banco-de-venezuela',
-                },
-                {
-                    label: 'Banco Mercantil',
-                    value: 'banco-mercantil',
-                },
-                {
-                    label: 'Banco Provincial',
-                    value: 'banco-provincial',
-                },
-                {
-                    label: 'Banco Bicentenario',
-                    value: 'banco-bicentenario',
-                },
-                {
-                    label: 'Banco Exterior',
-                    value: 'banco-exterior',
-                },
-                {
-                    label: 'Banco Occidental de Descuento',
-                    value: 'banco-occidental-de-descuento',
-                },
-                {
-                    label: 'Banco Sofitasa',
-                    value: 'banco-sofitasa',
-                },
-                {
-                    label: 'Banco Plaza',
-                    value: 'banco-plaza',
-                },
-                {
-                    label: 'Banco Caroní',
-                    value: 'banco-caroni',
-                },
-                {
-                    label: 'Banco Activo',
-                    value: 'banco-activo',
-                },
-                {
-                    label: 'Banco del Tesoro',
-                    value: 'banco-del-tesoro',
-                },
-                {
-                    label: 'Banco Agrícola de Venezuela',
-                    value: 'banco-agricola-de-venezuela',
-                },
-                {
-                    label: 'Banco de la Fuerza Armada Nacional Bolivariana',
-                    value: 'banco-de-la-fuerza-armada-nacional-bolivariana',
-                },
-                {
-                    label: 'Banco del Pueblo Soberano',
-                    value: 'banco-del-pueblo-soberano',
-                },
-                {
-                    label: 'Banco Nacional de Crédito',
-                    value: 'banco-nacional-de-credito',
-                },
-                {
-                    label: 'Banco Venezolano de Crédito',
-                    value: 'banco-venezolano-de-credito',
-                },
-                {
-                    label: 'Banesco',
-                    value: 'banesco',
-                }
-            ]
-        },
+        VenezuelanBanks(),
         {
             name: 'idn',
             type: 'number',
             required: true,
             label: 'Cédula de identidad',
         },
-        // {
-        //     name: 'referenceNumber',
-        //     type: 'text',
-        //     required: true,
-        //     label: 'Número de referencia',
-        // }
     ],
 }
 
