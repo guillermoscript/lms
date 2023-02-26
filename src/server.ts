318c7739b79571cf140937923c895b0bd98af168
guillermoscript <arepayquezo@gmail.com>
2023-02-26 17:20:01 -0400

    added smtp env to send emails

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,23 @@ payload.init({
   secret: process.env.PAYLOAD_SECRET,
   mongoURL: process.env.MONGODB_URI,
   express: app,
+  email: {
+    transportOptions: {
+      host: process.env.SMTP_HOST,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD
+      },
+      port: process.env.SMTP_PORT,
+      secure: true, // use TLS
+      tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false
+        }
+      },
+    fromName: 'LMS Payload Admin',
+    fromAddress: 'noreply@pincelx.com'
+  },
   onInit: () => {
     payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
   },
