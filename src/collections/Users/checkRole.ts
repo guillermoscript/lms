075f5ea155db1c703e3106c8af62151d878dfc06
guillermoscript <arepayquezo@gmail.com>
2023-02-26 16:51:18 -0400

    collections access utility for checking the roles

---
+++ src/collections/Users/checkRole.ts
@@ -0,0 +1,15 @@
+import type { User } from '../../payload-types'
+
+export const checkRole = (allRoles: User['roles'] = [], user?: User): boolean => {
+    if (!user) {
+        return false
+    }
+
+    const checkRoles = allRoles.some(role => {
+        return user?.roles.some(individualRole => {
+            return individualRole === role
+        })
+    })
+
+    return checkRoles
+}
