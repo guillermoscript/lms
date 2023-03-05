f933b89e24d919aa662fa8f447ec8bca59b4ac6a
guillermoscript <arepayquezo@gmail.com>
2023-03-05 18:58:28 -0400

    pivot table added

---
+++ src/collections/StudentHasExams.ts
@@ -0,0 +1,64 @@
+import payload from 'payload';
+import { CollectionConfig } from 'payload/types';
+import { isAdmin } from '../access/isAdmin';
+import { isAdminOrCreatedBy } from '../access/isAdminOrCreatedBy';
+import { isAdminOrTeacher } from '../access/isAdminOrTeacher';
+import { isEnrolledOrHasAccess } from '../access/isEnrolledOrHasAccess';
+import { createdByField } from '../fields/createdBy';
+import { lastModifiedBy } from '../fields/lastModifiedBy ';
+import { checkRole } from './Users/checkRole';
+
+// Example Collection - For reference only, this must be added to payload.config.ts to be used.
+const StudentHasExams: CollectionConfig = {
+    slug: 'student-has-exams',
+    admin: {
+        useAsTitle: 'name'
+    },
+    access: {
+        create: ({ req: { user } }) => {
+            if (checkRole(['admin', 'teacher'], user)) {
+                return true
+            }
+            return false
+        },
+        read: ({ req: { user } }) => isEnrolledOrHasAccess(['admin', 'editor', 'teacher'],user),
+        update: isAdminOrCreatedBy,
+        delete: isAdmin
+    },
+    fields: [
+        {
+            name: 'name',
+            type: 'text',
+            required: true,
+            label: 'Nombre del curso',
+        },
+        {
+            name: 'description',
+            type: 'text',
+            required: true,
+            label: 'Descripción del curso',
+        },
+        {
+            name: 'course',
+            type: 'relationship',
+            relationTo: 'courses',
+            hasMany: false,
+        },
+        {
+            name: 'endDate',
+            type: 'date',
+            required: true,
+            label: 'Fecha de finalización',
+        },
+        {
+            name: 'maxScore',
+            type: 'number',
+            required: true,
+            label: 'Puntaje máximo',
+        },
+        lastModifiedBy(),
+        createdByField()
+    ],
+}
+
+export default StudentHasExams;
