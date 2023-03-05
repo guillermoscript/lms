23eb1fc21e308415ba5f38d05e1cd346f1fd520b
guillermoscript <arepayquezo@gmail.com>
2023-03-05 14:25:11 -0400

    transaction added to its own folder and now details added

--- src/collections/Transactions/Transactions.ts
+++ src/collections/Transactions/Transactions.ts
@@ -1,7 +1,9 @@
 import { CollectionConfig } from 'payload/types';
-import { isAdmin } from '../access/isAdmin';
-import { isRole } from '../access/isRole';
-import { isSelfStudent } from '../access/isSelfStudent';
+import { anyone } from '../../access/anyone';
+import { isAdmin } from '../../access/isAdmin';
+import { isRole } from '../../access/isRole';
+import { isSelfStudent } from '../../access/isSelfStudent';
+import periodicity from '../../fields/periodicity';
 // Example Collection - For reference only, this must be added to payload.config.ts to be used.
 const Transactions: CollectionConfig = {
     slug: 'transactions',
@@ -9,6 +11,7 @@ const Transactions: CollectionConfig = {
         useAsTitle: 'id'
     },
     access: {
+        create: anyone,
         read:  (data) => {
             const {req: {user}} = data
             return isRole({ user, role: 'admin' }) || isRole({ user, role: 'editor' }) || isSelfStudent(data)
@@ -56,8 +59,26 @@ const Transactions: CollectionConfig = {
                 // 'certificates'
             ],
             hasMany: false,
+        },
+        periodicity(),
+        {
+            name: 'isSubscription',
+            type: 'checkbox',
+            defaultValue: true,
+            label: '¿Es una suscripción?',
+
+        },
+        {
+            name: 'details',
+            type: 'richText',
+            label: 'Detalles',
         }
     ],
+    hooks: {
+        // afterChange: async ({ operation, document, req }) => {
+        //     const status = document.status
+        // },
+    }
 }
 
 export default Transactions;
