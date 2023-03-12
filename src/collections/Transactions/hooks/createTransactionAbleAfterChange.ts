481536e99c220bc1a66a1a8a1e068c7be23de115
guillermoscript <arepayquezo@gmail.com>
2023-03-11 20:18:39 -0400

    sending email for transaction approve

--- src/collections/Transactions/hooks/createTransactionAbleAfterChange.ts
+++ src/collections/Transactions/hooks/createTransactionAbleAfterChange.ts
@@ -1,8 +1,10 @@
-import { Enrollment, Transaction, Subscription } from '../../../payload-types';
+import { Enrollment, Transaction, Subscription, User } from '../../../payload-types';
 import { Payload } from "payload";
 import createEnrollmentDto, { EnrollmentCreateDto } from '../dto/createEnrollmentDto';
 import createSubscriptionDto, {SubscriptionCreateDto} from '../dto/createSubscriptionDto';
 import { FieldHook } from 'payload/types';
+import { Message } from 'payload/dist/email/types';
+import tryCatch from '../../../utilities/tryCatch';
 
 const createTransactionAbleAfterChange: FieldHook = async ({
     req,
@@ -53,9 +55,28 @@ const createTransactionAbleAfterChange: FieldHook = async ({
     const typeOfTransaction = docType.transactionAble.relationTo
     const createDoc = stateMachine[typeOfTransaction]()
 
+    await sendUserEmail(docType.customer, req.payload)
+
     console.log(await createDoc, '<----------- LOOOOOOOOOOOOOOOOOOOOOOOOOOOK');
 }
 
+async function sendUserEmail(userId: Transaction['customer'], payload: Payload) {
+    console.log(userId, '<----------- userId');
+    const [user, userError] = await tryCatch<User>(payload.findByID({
+        collection: 'users',
+        id: userId as string,
+    }))
+    
+    if (userError) return
+    
+    const mailOptions: Message = {
+        from: 'noreply@pincelx.com',
+        to: user.email as string,
+        subject: 'Su compra ha sido exitosa',
+        html: `<h1>Gracias por comprar</h1>`,
+    };
+    payload.sendEmail(mailOptions)
+}
 async function createEnrollment(enrollmentData: EnrollmentCreateDto, payload: Payload): Promise<[Enrollment, Error]> {
     try {
         const enrollment: Enrollment = await payload.create({
