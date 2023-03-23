7f8e1c70aaa565a0f5655b96cf005bccca682f07
guillermoscript <arepayquezo@gmail.com>
2023-03-22 21:45:49 -0400

    removed old collections

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,23 +1,15 @@
 import { buildConfig } from 'payload/config';
 import path from 'path';
-// import Examples from './collections/Examples';
 import Users from './collections/Users/Users';
-import PagoMovils from './collections/PagoMovils';
 import Orders from './collections/Orders/Orders';
 import PaymentMethods from './collections/PaymentMethods';
-import Zelles from './collections/Zelles';
 import Courses from './collections/Courses';
 import Currencies from './collections/Currencies';
 import Enrollments from './collections/Enrollments';
 import Subscriptions from './collections/Subscriptions';
-import ProductPrices from './collections/ProductPrices';
 import Products from './collections/Products';
 import Evaluations from './collections/Evaluations';
-import Exams from './collections/Exams';
-import Homeworks from './collections/Homeworks';
-import Lessons from './collections/Lessons';
 import Plans from './collections/Plans';
-import Customers from './collections/Customers';
 import Media from './collections/Media';
 
 export default buildConfig({
@@ -27,23 +19,16 @@ export default buildConfig({
   },
   collections: [
     Courses,
-    Customers,
     Currencies,
     Enrollments,
     Evaluations,
-    Exams,
-    Homeworks,
     Media,
-    Lessons,
-    PagoMovils,
     PaymentMethods,
     Plans,
-    ProductPrices,
     Products,
     Subscriptions,
     Orders,
     Users,
-    Zelles,
   ],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
