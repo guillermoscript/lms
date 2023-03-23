b28aedbd9120238e8eccdc3d969ff9d1872b7e00
guillermoscript <arepayquezo@gmail.com>
2023-03-22 23:20:38 -0400

    new collections

---
+++ src/collections/Comments.ts
@@ -0,0 +1,66 @@
+import { CollectionConfig } from 'payload/types';
+import { isAdminOrCreatedBy } from '../access/isAdminOrCreatedBy';
+import { createdByField } from '../fields/createdBy';
+import { lastModifiedBy } from '../fields/lastModifiedBy ';
+import { populateCreatedBy } from '../hooks/populateCreatedBy';
+import { populateLastModifiedBy } from '../hooks/populateLastModifiedBy';
+
+const Comments: CollectionConfig = {
+    slug: 'comments',
+    admin: {
+        useAsTitle: 'name'
+    },
+    access: {
+        create: isAdminOrCreatedBy,
+        // TODO: Only active subscriptions can access this
+        read: () => true,
+        update: isAdminOrCreatedBy,
+        delete: isAdminOrCreatedBy
+    },
+    fields: [
+        {
+            name: 'comment',
+            type: 'text',
+            required: false,
+        },
+        {
+            name: 'user',
+            type: 'relationship',
+            relationTo: 'users',
+            hasMany: false,
+        },
+        {
+            name: 'likes',
+            type: 'number',
+            required: false,
+        },
+        {
+            name: 'dislikes',
+            type: 'number',
+            required: false,
+        },
+        {
+            name: 'lesson',
+            type: 'relationship',
+            relationTo: 'lessons',
+            hasMany: false,
+        },
+        {
+            name: 'replyTo',
+            type: 'relationship',
+            relationTo: 'comments',
+            hasMany: true,
+        },
+        createdByField(),
+        lastModifiedBy(),
+    ],
+
+    hooks: {
+        beforeChange: [
+            populateCreatedBy,
+            populateLastModifiedBy
+        ]
+    }
+};
+
+export default Comments;
---
+++ src/collections/Lessons.ts
@@ -0,0 +1,110 @@
+import { CollectionConfig } from 'payload/types';
+import { isAdmin } from '../access/isAdmin';
+import { isAdminOrCreatedBy } from '../access/isAdminOrCreatedBy';
+import { isAdminOrTeacher } from '../access/isAdminOrTeacher';
+import { categoryField } from '../fields/category';
+import { createdByField } from '../fields/createdBy';
+import { isPublicField } from '../fields/isPublic';
+import { lastModifiedBy } from '../fields/lastModifiedBy ';
+import { populateCreatedBy } from '../hooks/populateCreatedBy';
+import { populateLastModifiedBy } from '../hooks/populateLastModifiedBy';
+import { populateTeacher } from '../hooks/poupulateTeacherField';
+
+
+// Example Collection - For reference only, this must be added to payload.config.ts to be used.
+const Lessons: CollectionConfig = {
+    slug: 'lessons',
+    admin: {
+        useAsTitle: 'name'
+    },
+    access: {
+        create: isAdminOrTeacher,
+        // TODO: Only active subscriptions can access this
+        read: () => true,
+        update: isAdminOrCreatedBy,
+        delete: isAdmin
+    },
+    fields: [
+        {
+            name: 'name',
+            type: 'text',
+            required: true,
+            label: 'Nombre del curso',
+        },
+        {
+            name: 'description',
+            type: 'text',
+            required: true,
+            label: 'Descripción del curso',
+        },
+        categoryField(),
+        {
+            name: "teacher",
+            type: "relationship",
+            relationTo: "users",
+            hasMany: false,
+            label: "Profesor",
+            access: {
+                read: ({ req }) => {
+                    if (!req.user) {
+                        return false
+                    }
+                    if (req.user.roles?.includes('teacher')) {
+                        return false
+                    }
+                    return true
+                }
+            },
+            filterOptions: ({ relationTo, siblingData, user }) => {
+                return {
+                    roles: {
+                        contains: 'teacher' || 'admin'
+                    }
+                }
+            }
+        },
+        {
+            name: 'content',
+            type: 'richText',
+            required: true,
+            label: 'Contenido',
+        },
+        {
+            name: 'resources',
+            type: 'array',
+            label: 'Recursos',
+            fields: [
+                {
+                    name: 'name',
+                    type: 'text',
+                    required: true,
+                    label: 'Nombre del recurso',
+                },
+                {
+                    name: 'description',
+                    type: 'richText',
+                    required: true,
+                    label: 'Descripción del recurso',
+                },
+            ],
+        },
+        {
+            name: 'comments',
+            type: 'relationship',
+            relationTo: 'comments',
+            hasMany: true,
+        },
+        createdByField(),
+        lastModifiedBy(),
+        isPublicField()
+    ],
+    hooks: {
+        beforeChange: [
+            populateTeacher,
+            populateCreatedBy,
+            populateLastModifiedBy
+        ]
+    }
+}
+
+export default Lessons;
