6f93f5f84cdb3876263129ad89b3e7d168387814
guillermoscript <arepayquezo@gmail.com>
2023-03-22 21:41:02 -0400

    added sub type of payment method inside of collection

--- src/collections/PaymentMethods.ts
+++ src/collections/PaymentMethods.ts
@@ -1,6 +1,7 @@
 import { CollectionConfig } from 'payload/types';
 import { isAdminOrSelf } from '../access/isAdminOrSelf';
 import { createdByField } from '../fields/createdBy';
+import VenezuelanBanks from '../fields/VenezuelanBanks';
 import { populateCreatedBy } from '../hooks/populateCreatedBy';
 import { populateLastModifiedBy } from '../hooks/populateLastModifiedBy';
 
@@ -25,15 +26,146 @@ const PaymentMethods: CollectionConfig = {
             hasMany: true,
         },
         {
+            name: 'title',
+            type: 'text',
+            required: true,
+            label: 'Título',
+        },
+        {
             name: 'paymentMethodType',
-            type: 'relationship',
+            type: 'radio',
+            required: true,
             label: 'Tipo de pago',
-            relationTo: [
-                'zelles', 
-                // 'paypal', 
-                'pago-movils', 
-                // 'cash', 
-                // 'bank-transfer'
+            options: [
+                {
+                    label: 'Zelle',
+                    value: 'zelle',
+                },
+                {
+                    label: 'Paypal',
+                    value: 'paypal',
+                },
+                {
+                    label: 'Pago móvil',
+                    value: 'pago-movil',
+                },
+                {
+                    label: 'Efectivo',
+                    value: 'cash',
+                },
+                {
+                    label: 'Transferencia bancaria',
+                    value: 'bank-transfer',
+                },
+            ],
+        },
+        {
+            name: 'zelle',
+            type: 'group',
+            label: 'Zelle',
+            admin: {
+                condition: (data) => data.paymentMethodType === 'zelle'
+            },
+            fields: [
+                {
+                    name: 'zelleEmail',
+                    type: 'text',
+                    label: 'Correo electrónico de Zelle',
+                },
+                {
+                    name: 'fullName',
+                    type: 'text',
+                    label: 'Nombre completo',
+                },
+            ],
+        },
+        {
+            name: 'paypal',
+            type: 'group',
+            admin: {
+                condition: (data) => data.paymentMethodType === 'paypal'
+            },
+            label: 'Paypal',
+            fields: [
+                {
+                    name: 'paypalEmail',
+                    type: 'text',
+                    label: 'Correo electrónico de Paypal',
+                },
+            ],
+        },
+        {
+            name: 'pagoMovil',
+            type: 'group',
+            label: 'Pago móvil',
+            admin: {
+                condition: (data) => data.paymentMethodType === 'pago-movil'
+            },
+            fields: [
+                {
+                    name: 'phoneNumber',
+                    type: 'text',
+                    required: true,
+                    label: 'Número de teléfono',
+                },
+                VenezuelanBanks(),
+                {
+                    name: 'idn',
+                    type: 'number',
+                    required: true,
+                    label: 'Cédula de identidad',
+                },
+            ],
+        },
+        {
+            name: 'cash',
+            type: 'group',
+            label: 'Efectivo',
+            admin: {
+                condition: (data) => data.paymentMethodType === 'cash'
+            },
+            fields: [
+                {
+                    name: 'cash',
+                    type: 'text',
+                    label: 'Efectivo',
+                },
+            ],
+        },
+        {
+            name: 'bankTransfer',
+            type: 'group',
+            admin: {
+                condition: (data) => data.paymentMethodType === 'bank-transfer'
+            },
+            label: 'Transferencia bancaria',
+            fields: [
+                {
+                    name: 'accountNumber',
+                    type: 'text',
+                    label: 'Número de cuenta',
+                },
+                {
+                    name: 'bankName',
+                    type: 'text',
+                    label: 'Nombre del banco',
+                },
+                {
+                    name: 'accountType',
+                    type: 'radio',
+                    label: 'Tipo de cuenta',
+                    options: [
+                        {
+                            label: 'Ahorro',
+                            value: 'savings',
+                        },
+                        {
+                            label: 'Corriente',
+                            value: 'current',
+                        },
+                    ],
+                },
+                VenezuelanBanks(),
             ],
         },
         createdByField()
