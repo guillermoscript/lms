3fdbc7435dfa5493003cabb0b948d2751faa942c
guillermoscript <arepayquezo@gmail.com>
2023-07-22 17:26:24 -0400

    moved to its own function

--- src/server.ts
+++ src/server.ts
@@ -12,58 +12,53 @@ app.get('/', (_, res) => {
   res.redirect('/admin');
 });
 
-const secret = process.env.PAYLOAD_SECRET
-const mongoURL = process.env.MONGODB_URI
+async function start() {
 
-if (!secret) {
-  throw new Error('Missing env var: PAYLOAD_SECRET')
-}
+  const secret = process.env.PAYLOAD_SECRET
+  const mongoURL = process.env.MONGODB_URI
 
-if (!mongoURL) {
-  throw new Error('Missing env var: MONGODB_URI')
-}
-
-// Initialize Payload
-payload.init({
-  secret,
-  mongoURL,
-  express: app,
-  email: {
-    transportOptions: {
-      host: process.env.SMTP_HOST,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD
-      },
-      port: process.env.SMTP_PORT,
-      secure: true, // use TLS
-      tls: {
-        // do not fail on invalid certs
-        rejectUnauthorized: false
-      }
-    },
-    fromName: 'LMS Payload Admin',
-    fromAddress: noReplyEmail
-  },
-  onInit: () => {
-    payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
-  },
-})
-
-// Add your own express routes here
-
-app.get('/cron', async (req, res) => {
-  try {
-    const result = await runInactivateSubscriptionAndCreateRenewalOrder();
-    console.log(result, '<----------- result');
-    res.status(StatusCodes.OK).send(result);
-  } catch (error) {
-    console.log(error, '<----------- error');
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+  if (!secret) {
+    throw new Error('Missing env var: PAYLOAD_SECRET')
   }
+
+  if (!mongoURL) {
+    throw new Error('Missing env var: MONGODB_URI')
+  }
+
+  await payload.init({
+    secret,
+    mongoURL,
+    express: app,
+    email: {
+      transportOptions: {
+        host: process.env.SMTP_HOST,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD
+        },
+        port: process.env.SMTP_PORT,
+        secure: true, // use TLS
+        tls: {
+          // do not fail on invalid certs
+          rejectUnauthorized: false
+        }
+      },
+      fromName: 'LMS Payload Admin',
+      fromAddress: noReplyEmail
+    },
+    onInit: () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+    },
+  })
+
+  app.listen(process.env.PORT, () => {
+    payload.logger.info(`Server running on port ${process.env.PORT}`)
+  });
+}
+
+start().catch(error => {
+  console.error(error)
+  process.exit(1)
 })
 
-app.listen(process.env.PORT);
-// app.listen(process.env.PORT, () => {
-//   payload.logger.info(`Server running on port ${process.env.PORT}`)
-// });
+
