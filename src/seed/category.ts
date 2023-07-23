bc839d21a134092ac30870a911b7505d741cad52
guillermoscript <arepayquezo@gmail.com>
2023-07-23 19:14:28 -0400

    category seed added

---
+++ src/seed/category.ts
@@ -0,0 +1,16 @@
+export const categoriesData = [
+    {
+        collection: 'categories',
+        data: {
+            name: 'Programacion',
+            description: 'Programacion description',
+        }
+    },
+    {
+        collection: 'categories',
+        data: {
+            name: 'Marketing',
+            description: 'Marketing description',
+        }
+    }
+]
