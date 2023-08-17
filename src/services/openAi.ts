270c3442ed6cf09f1328696085e57f9661273993
guillermoscript <arepayquezo@gmail.com>
2023-08-16 20:58:30 -0400

    open ai work in progress

---
+++ src/services/openAi.ts
@@ -0,0 +1,56 @@
+
+import { Configuration, OpenAIApi } from 'openai';
+import tryCatch from '../utilities/tryCatch';
+
+export const historyTeacherPrompt = `Quiero que actúes como historiador.Investigarás y analizarás acontecimientos culturales, económicos, políticos y sociales del pasado, recopilarás datos de fuentes primarias y los utilizarás para elaborar teorías sobre lo que ocurrió durante diversos periodos de la historia. Tu trabajo es evaluar preguntas y respuestas que te voy a presentar, tienes que evaluarlas cada una y luego dar al tema una puntuación de 0 a 20 y luego articular tu puntuación sobre la respuesta para enseñar al estudiante, si esta es incorrecta debe instruir al estudiante cual es la respuesta deseada, al final da un resumen instructivo al estudiante.la respuesta final debe ser un JSON con el siguiente type de typescript 
+type TeacherResponse = {
+    finalScore: number;
+    finalComment: string;
+    examn: Array<{
+        score: number;
+        comment: string;
+    }>;
+}`
+
+export function openAiService({
+    initialPrompt,
+    prompt
+}: {
+    prompt: string
+    initialPrompt?: string
+}) {
+
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const openai = new OpenAIApi(configuration);
+
+    async function chatCompletition() {
+
+        const [completion, err] = await tryCatch(openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{
+                "role": "system",
+                "content": initialPrompt
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }],
+        }));
+
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        console.log(completion.data)
+        const response = completion.data.choices[0].message
+        return response
+    }
+
+    return {
+        chatCompletition
+    }
+}
