ab81289b0e30a517328642585bc148e8dcf86940
guillermoscript <arepayquezo@gmail.com>
2023-08-17 21:25:39 -0400

    followind docs comment this works ass students dont have access to the admin

--- src/access/hasAccessOrIsEnrolled.ts
+++ src/access/hasAccessOrIsEnrolled.ts
@@ -2,7 +2,7 @@ import { Access } from "payload/config"
 import { checkRole } from "../collections/Users/checkRole"
 import { User } from "../payload-types"
 
-const hasAccessOrIsEnrolled= ({ req: { user, payload }, id }: { req: { user: User, payload: any }, id?: string | number }, type: string): boolean | Promise<boolean> => {
+const hasAccessOrIsEnrolled = async ({ req: { user, payload }, id }: { req: { user: User, payload: any }, id?: string | number }, type: string) => {
 
     if (!user) {
         return false;
@@ -12,7 +12,8 @@ const hasAccessOrIsEnrolled= ({ req: { user, payload }, id }: { req: { user: Use
         return true;
     }
 
-    async function findIfUserIsEnrolled() {
+    if (id) {
+
         try {
             const enrollment = await payload.find({
                 collection: 'enrollments',
@@ -41,14 +42,22 @@ const hasAccessOrIsEnrolled= ({ req: { user, payload }, id }: { req: { user: Use
                 return false;
             }
 
+            console.log('its true333', enrollment.docs[0]);
+
             return true;
         } catch (error) {
             console.log(error);
             return false;
         }
-    }
+    } else {
+        // leave it true because of comment in docs
+        // If you use id or data within your access control functions, make sure to check that they are defined first.
+        // If they are not, then you can assume that your access control is being executed via the access operation, to determine solely what the user can do within the Admin UI.
 
-    return findIfUserIsEnrolled();
+        // and becuase i restric access to the collection in the admin config
+        // i can do this 
+        return true
+    }
 };
 
 export default hasAccessOrIsEnrolled
