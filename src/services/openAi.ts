74dc1c8474bfb4d73dc9d213e628147459fcf62a
guillermoscript <arepayquezo@gmail.com>
2023-08-18 23:17:43 -0400

    still on work

--- src/services/openAi.ts
+++ src/services/openAi.ts
@@ -2,6 +2,7 @@
 import { Configuration, OpenAIApi } from 'openai';
 import tryCatch from '../utilities/tryCatch';
 
+// require('dotenv').config();
 export const historyTeacherPrompt = `Quiero que actúes como historiador.Investigarás y analizarás acontecimientos culturales, económicos, políticos y sociales del pasado, recopilarás datos de fuentes primarias y los utilizarás para elaborar teorías sobre lo que ocurrió durante diversos periodos de la historia. Tu trabajo es evaluar preguntas y respuestas que te voy a presentar, tienes que evaluarlas cada una y luego dar al tema una puntuación de 0 a 20 y luego articular tu puntuación sobre la respuesta para enseñar al estudiante, si esta es incorrecta debe instruir al estudiante cual es la respuesta deseada, al final da un resumen instructivo al estudiante.la respuesta final debe ser un JSON con el siguiente type de typescript 
 type TeacherResponse = {
     finalScore: number;
@@ -12,13 +13,9 @@ type TeacherResponse = {
     }>;
 }`
 
-export function openAiService({
-    initialPrompt,
-    prompt
-}: {
-    prompt: string
-    initialPrompt?: string
-}) {
+export function openAiService() {
+
+    console.log(process.env.OPENAI_API_KEY, 'process.env.OPENAI_API_KEY')
 
     const configuration = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
@@ -26,7 +23,13 @@ export function openAiService({
 
     const openai = new OpenAIApi(configuration);
 
-    async function chatCompletition() {
+    async function chatCompletition({
+        initialPrompt,
+        prompt
+    }: {
+        prompt: string
+        initialPrompt?: string
+    }) {
 
         const [completion, err] = await tryCatch(openai.createChatCompletion({
             model: "gpt-3.5-turbo",
@@ -38,16 +41,20 @@ export function openAiService({
                 "role": "user",
                 "content": prompt
             }],
+            temperature: 0.11,
+            max_tokens: 10324,
+            top_p: 1,
+            frequency_penalty: 0.34,
+            presence_penalty: 0.25,
         }));
 
         if (err) {
             console.log(err)
-            return
+            return [null, err]
         }
 
-        console.log(completion.data)
-        const response = completion.data.choices[0].message
-        return response
+        const response = completion?.data.choices[0].message?.content
+        return [response, null]
     }
 
     return {
